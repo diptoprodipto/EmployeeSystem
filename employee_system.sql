b0b27cf6d2e857ad33dfef7f0f40ab937ee9d69f
diptoprodipto <diptoprodipto@gmail.com>
2022-06-22 17:19:45 +0600

    added database

---
+++ employee_system.sql
@@ -0,0 +1,130 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jun 22, 2022 at 01:19 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `employee_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `id` int(11) NOT NULL,
+  `employee_id` bigint(20) NOT NULL,
+  `date` datetime NOT NULL,
+  `working_status` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`id`, `employee_id`, `date`, `working_status`, `created_at`) VALUES
+(1, 107, '2022-06-22 16:15:00', 1, '2022-06-22 10:15:23'),
+(2, 109, '2022-06-22 16:15:00', 1, '2022-06-22 10:15:23'),
+(3, 107, '2022-06-22 17:22:00', 1, '2022-06-22 10:22:24'),
+(4, 109, '2022-06-22 18:21:00', 1, '2022-06-22 10:22:24'),
+(5, 107, '2022-06-22 16:26:00', 1, '2022-06-22 10:26:17'),
+(6, 109, '2022-06-22 16:26:00', 1, '2022-06-22 10:26:17'),
+(7, 107, '2022-06-22 16:26:00', 1, '2022-06-22 10:27:01'),
+(8, 109, '2022-06-22 16:26:00', 1, '2022-06-22 10:27:01'),
+(9, 107, '2022-06-22 16:27:00', 1, '2022-06-22 10:27:43'),
+(10, 109, '2022-06-22 16:27:00', 1, '2022-06-22 10:27:43'),
+(11, 107, '2022-06-22 16:38:00', 1, '2022-06-22 10:38:04'),
+(12, 109, '2022-06-22 16:38:00', 1, '2022-06-22 10:38:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `employee_id` bigint(20) NOT NULL,
+  `employee_name` varchar(255) NOT NULL,
+  `designation` varchar(255) NOT NULL,
+  `role` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `salary` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL,
+  `resignation_date` datetime DEFAULT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`employee_id`, `employee_name`, `designation`, `role`, `email`, `phone`, `salary`, `status`, `resignation_date`, `password`) VALUES
+(107, 'Aslam', 'SWE', 2, 'aslam@gmail.com', '01521202799', '40000', 0, '0000-00-00 00:00:00', '1234'),
+(109, 'Kismet', 'SWE', 1, 'kismet@gmail.com', '01793317617', '40000', 0, '0000-00-00 00:00:00', '1234');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `foreign_key` (`employee_id`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`employee_id`),
+  ADD UNIQUE KEY `unique_email` (`email`(200));
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `employee_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD CONSTRAINT `foreign_key` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
